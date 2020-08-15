9f342b9c5d0d3c0ba0a86f02332c06af702b5636
farinap5 <62824857+farinap5@users.noreply.github.com>
2020-08-14 22:42:34 -0300

    Update descoverdom.py

--- mods/descoverdom.py
+++ mods/descoverdom.py
@@ -20,58 +20,53 @@ def descoverdom():
 
     domwho = whois.query(dom)
 
-
+    domht = "http://" + dom
 
     linha()
-    print("Ip Address:      ",ipaddr)
-    print("Service:         ",domwho.name)
-    print("Expiration Date: ",domwho.expiration_date)
-    print("Creation Date:   ",domwho.creation_date)
-    print("Last Update:     ",domwho.last_updated)
-    #print("Owner:           ",domwho.owner)
-    print("Regist:          ",domwho.registrar)
-    print("Status:          ",domwho.status)
-    print("Server:          ",domwho.name_servers)
 
-    try:
-        linha()
-        print("\033[1;32mHeaders Response\033[0;0m")
-        domht = "http://"+ dom
-        donthh = "http://api.hackertarget.com/httpheaders/?q=" + dom
-        print(requests.get(donthh).text)
-    except:
-        pass
+    cf = requests.get(domht).text
+    c1 = '<div class="cf-browser-verification cf-im-under-attack">'
+    c2 = 'Checking your browser before accessing'
+    c3 = 'Please enable Cookies and reload the page'
+    c4 = 'DDoS protection by'
+
+    if c1 and c2 and c3 and c4 in cf:
+        print("  Cloud Flare: True")
+        print("  Would you like to continue")
+    else:
+        print("  No Cloud Flare")
+
+    print("  Ip Address:      ",ipaddr)
+    print("  Service:         ",domwho.name)
+    print("  Expiration Date: ",domwho.expiration_date)
+    print("  Creation Date:   ",domwho.creation_date)
+    print("  Last Update:     ",domwho.last_updated)
+    print("  Name Servers:    ",domwho.name_servers)
+    print("  Register:        ",domwho.registrar)
+
+    linha()
 
     f = request.urlopen(domht)
-    print(f.getcode())
+    print("  Code: ",f.getcode())
     print(f.info())
 
     linha()
-    print("\033[1;32mRedirection\033[0;0m")
-    response = requests.get(domht)
-    if response.is_redirect == False:
-        print("No Redirection")
-    else:
-        print("Web Site Redirecting")
 
-    try:
-        linha()
-        print("\033[1;32mDNS Lookup\033[0;0m")
-        print(requests.get("https://api.hackertarget.com/mtr/?q=" + dom).text)
-    except:
+    red = requests.get(domht)
+    print("Redirecting:           ",red.is_redirect)
+    print("Redirecting Permanent: ",red.is_permanent_redirect)
+    print("Encoding:              ",red.apparent_encoding)
+    print("Next:                  ",red.next)
+
+    zin = requests.get(domht).text
+    if "<form" and "</form>" in zin:
+        print("Form Found")
+    else:
         pass
-    try:
-        linha()
-        print("\033[1;32mHost Search\033[0;0m")
-        print(requests.get("https://api.hackertarget.com/hostsearch/?q=" + dom).text)
-    except:
-        pass
-    try:
-        linha()
-        print("\033[1;32mFind Shared DNS\033[0;0m")
-        print(requests.get("https://api.hackertarget.com/findshareddns/?q=" + dom).text)
-    except:
-        pass
+
+
+
+
     input("press enter")
     a_main.a_main()
 
