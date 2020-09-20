0a098556180b3d4accd26c47cea5e5e0701753ae
farinap5 <62824857+farinap5@users.noreply.github.com>
2020-09-20 12:09:14 -0300

    Add files via upload

--- mods/hashtool.py
+++ mods/hashtool.py
@@ -5,13 +5,14 @@ import random
 #from mods import a_main
 
 def hashkillr():
-    s = "abcdefghijklmnopqrstuvwxyz"
+    s = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     passlen = 5
 
-    key = "21232f297a57a5a743894a0e4a801fc3"
+    #key = "21232f297a57a5a743894a0e4a801fc3" - admin
     print("   Hash MD5 BruteForce Random Value")
     print("   Set Hash ")
-    #key = input(" > ")
+    key = input("  > ")
+    passlen = int(input("  password length: "))
     print("   [\033[1;32mOK\033[0;0m] - ", key)
 
 
@@ -34,19 +35,22 @@ def hashkillr():
             break
 
 def hashkillw():
-    print("still in test, set your Word List")
-    arq = " "
-    key = "21232f297a57a5a743894a0e4a801fc3"
+
+
+    #key = "21232f297a57a5a743894a0e4a801fc3"
     print("   Hash MD5 BruteForce")
     print("   Set Hash ")
-    #key = input(" > ")
+    key = input("  > ")
+    arq = input("  set WordList: ")
     print("   [\033[1;32mOK\033[0;0m] - ", key)
 
+    mywl = []
 
-    file = open(arq).readlines()
+    file = open(arq).read().splitlines()
+    for line in file:
+        mywl.append(line)
 
-
-    for l in file:
+    for l in mywl:
         try:
             h = hashlib.md5(l)
             cat = h.hexdigest()
