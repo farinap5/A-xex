86a13b70746569d4ed6e04bb2487e9e2b95a433d
farinap5 <62824857+farinap5@users.noreply.github.com>
2021-01-04 20:56:57 -0300

    Update alogo.py

--- mods/alogo.py
+++ mods/alogo.py
@@ -4,5 +4,5 @@ def logo():
     print('   /____\   ___  \ /   | ____ /    \ /    ')
     print('  /      \       / \   |           / \    ')
     print(' /        \     /   \   \_____/   /   \   ')
-    print('\033[42;1;33m' + '             A-xex 1.3 is up              ' + '\033[0;0m')
+    print('\033[42;1;33m' + '             A-xex 1.4 is up              ' + '\033[0;0m')
     print('\033[34m', 'Author not responsible for malicious use!', '\033[0;0m')
